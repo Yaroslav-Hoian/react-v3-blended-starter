1125ebbf46e5c5d613bae5d8abd03fb765e33143
Yaroslav Hoian <sgoyan1405@gmail.com>
2025-09-09 13:28:20 +0300

    add props to Section component

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,5 +1,6 @@
+import type { ContainerProps } from "../../types/photo";
 import style from "./Section.module.css";
 
-export default function Section({ children }) {
+export default function Section({ children }: ContainerProps) {
   return <section className={style.section}>{children}</section>;
 }
